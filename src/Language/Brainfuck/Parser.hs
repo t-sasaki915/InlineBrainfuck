c9d832d7ad67c22b7fc30be019193b5b0544b4bc
Toma Sasaki <netst915@gmail.com>
2025-06-20 01:21:23 +0900

    add parser

---
+++ src/Language/Brainfuck/Parser.hs
@@ -0,0 +1,53 @@
+module Language.Brainfuck.Parser
+    ( BrainfuckToken (..)
+    , parseBrainfuck
+    , incrementToken
+    , decrementToken
+    , pointerIncrementToken
+    , pointerDecrementToken
+    , loopToken
+    , outputToken
+    ) where
+
+import           Control.Monad (void)
+import           Data.Functor  (($>))
+import           Data.Text     (Text)
+import           Text.Parsec
+
+data BrainfuckToken = IncrementToken
+                    | DecrementToken
+                    | PointerIncrementToken
+                    | PointerDecrementToken
+                    | LoopToken [BrainfuckToken]
+                    | OutputToken
+
+parseBrainfuck :: Parsec Text () [BrainfuckToken]
+parseBrainfuck = many $
+    ignoreUntilValidToken *>
+    try incrementToken <|>
+    try decrementToken <|>
+    try pointerIncrementToken <|>
+    try pointerDecrementToken <|>
+    try loopToken <|>
+    outputToken
+
+incrementToken :: Parsec Text () BrainfuckToken
+incrementToken = char '+' $> IncrementToken
+
+decrementToken :: Parsec Text () BrainfuckToken
+decrementToken = char '-' $> DecrementToken
+
+pointerIncrementToken :: Parsec Text () BrainfuckToken
+pointerIncrementToken = char '>' $> PointerIncrementToken
+
+pointerDecrementToken :: Parsec Text () BrainfuckToken
+pointerDecrementToken = char '<' $> PointerDecrementToken
+
+loopToken :: Parsec Text () BrainfuckToken
+loopToken = char '[' *> (LoopToken <$> parseBrainfuck) <* char ']'
+
+outputToken :: Parsec Text () BrainfuckToken
+outputToken = char '.' $> OutputToken
+
+ignoreUntilValidToken :: Parsec Text () ()
+ignoreUntilValidToken = void $ manyTill anyChar (oneOf "+-[]<>.")
